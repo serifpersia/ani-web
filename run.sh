e0470e6ccdf9f96becc13d9782e3dcd32c93ff49
Šerif Rami <ramiserifpersia@gmail.com>
2025-09-21 15:16:17 +0200

    restructure

--- run.sh
+++ run.sh
@@ -2,8 +2,7 @@
 
 clear
 
-
-echo -e "\033[1;33m-----------------------------------------------[0m"
+echo -e "\033[1;33m-----------------------------------------------\033[0m"
 echo -e "                    \033[1;36mani-web\033[0m"
 echo -e "\033[1;33m-----------------------------------------------\033[0m"
 echo -e "    \033[1;34mhttps://github.com/serifpersia/ani-web\033[0m"
@@ -12,4 +11,21 @@ echo
 echo -e "\033[1;32mStarting ani-web...\033[0m"
 echo
 
-node start-dev.js
+if [ ! -d "node_modules" ]; then
+    echo -e "\033[1;33mServer dependencies not found. Installing with npm install...\033[0m"
+    npm install
+    if [ $? -ne 0 ]; then
+        echo -e "\033[1;31mError: npm install failed!\033[0m"
+        read -p "Press Enter to exit..."
+        exit $?
+    else
+        echo -e "\033[1;32mSuccess: npm install completed!\033[0m"
+    fi
+else
+    echo -e "\033[1;36mFound existing dependencies (node_modules). Skipping install.\033[0m"
+fi
+echo
+
+echo -e "\033[1;33mBuilding and running ani-web...\033[0m"
+npm run build
+npm start
