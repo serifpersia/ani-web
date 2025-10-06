99540c31d737928cdf1fdd3f78e73cc0ecd37b44
Šerif Rami <ramiserifpersia@gmail.com>
2025-10-06 12:24:04 +0200

    cover art resizing in server optimization

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,23 +8,23 @@ export const fixThumbnailUrl = (url: string | undefined): string => {
 
   let finalUrl: string;
   if (url.startsWith('https://ytimgf.youtube-anime.com/images/')) {
-    finalUrl = url.replace('https://ytimgf.youtube-anime.com/images/', 'https://wp.youtube-anime.com/aln.youtube-anime.com/') + '?w=250';
+    finalUrl = url.replace('https://ytimgf.youtube-anime.com/images/', 'https://wp.youtube-anime.com/aln.youtube-anime.com/') + '?w=200';
   } else if (url.startsWith('https://cdnimg.xyz')) {
-    finalUrl = `https://wp.youtube-anime.com/${url.substring('https://'.length)}?w=250`;
+    finalUrl = `https://wp.youtube-anime.com/${url.substring('https://'.length)}?w=200`;
   } else if (url.startsWith('https://aln.youtube-anime.com')) {
-    finalUrl = url.replace('https://aln.youtube-anime.com/', 'https://wp.youtube-anime.com/aln.youtube-anime.com/images/') + '?w=250';
+    finalUrl = url.replace('https://aln.youtube-anime.com/', 'https://wp.youtube-anime.com/aln.youtube-anime.com/images/') + '?w=200';
   } else if (url.startsWith('__Show__')) {
-    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/images/${url}?w=250`;
+    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/images/${url}?w=200`;
   } else if (url.startsWith('mcovers')) {
     finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/${url}`;
   } else if (url.startsWith('https://gogocdn.net')) {
-    finalUrl = `https://wp.youtube-anime.com/${url.substring('https://'.length)}?w=250`;
+    finalUrl = `https://wp.youtube-anime.com/${url.substring('https://'.length)}?w=200`;
   } else if (url.startsWith('http')) {
-    finalUrl = `/api/image-proxy?url=${encodeURIComponent(url)}`;
+    finalUrl = `/api/image-proxy?url=${encodeURIComponent(url)}&w=200`;
   } else if (url.startsWith('images2')) {
-    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/${url}?w=250`;
+    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/${url}?w=200`;
   } else {
-    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/images/${url}?w=250`;
+    finalUrl = `https://wp.youtube-anime.com/aln.youtube-anime.com/images/${url}?w=200`;
   }
 
   thumbnailCache.set(url, finalUrl);
