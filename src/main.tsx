1dad6bed9d1ce780474ce1c9b0a787046032d6df
Šerif Rami <ramiserifpersia@gmail.com>
2025-10-26 00:09:54 +0200

    feat: Persist query cache to localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,23 @@ import './index.css';
 import { SidebarProvider } from './contexts/SidebarProvider';
 import { TitlePreferenceProvider } from './contexts/TitlePreferenceContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
+
+const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage });
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
